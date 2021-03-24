012878d475f6bbac1b7c2f5b1e54e025bc5c9800
nichol alexander <nichol.alexander@gmail.com>
2021-03-24 09:58:09 -0400

    initial commit

---
+++ lib/mimemagik.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "mimemagik/version"
+
+module Mimemagik
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/mimemagik/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Mimemagik
+  VERSION = "0.1.0"
+end
